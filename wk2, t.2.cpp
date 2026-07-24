35c61f35f8359f583af8bc85f8e764db808f4ed4
lawrenceonyoni <lawrenceonyoni@zetech.ac.ke>
2026-07-24 13:54:13 +0300

    Update wk2, t.2.cpp

--- wk2, t.2.cpp
+++ wk2, t.2.cpp
@@ -2,24 +2,30 @@
 //Task 2: Admission Decision (Nested if)  
 
 #include <iostream>
-#include <cstring>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	string student_name;
-	int age;
-	float exam_score;
-	
-	cout<<" Enter student name, age and exam score (0-100): "<<endl;
-	cin>>student_name>>age>>exam_score;
-	
-	if (age>=18)
-		if (exam_score>=50)
-		{
-		   cout<<"Admitted"<<endl;
-	
-	
-	
+    string student_name;
+    int age;
+    float exam_score;
+
+    cout << "Enter student name: ";
+    getline(cin, student_name);
+
+    cout << "Enter age and exam score: ";
+    cin >> age >> exam_score;
+
+    if (age >= 18 && exam_score >= 50) 
+    {
+        cout << "Admitted" << endl;
+    } 
+    else 
+    {
+        cout << "Not reach conditions given" << endl;
+    }
+
+    return 0;
 }
